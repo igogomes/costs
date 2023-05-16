a534e0b0ad4b2b1ebee58a80ecc42c6bf1f83f48
Igo Gomes <igogomes0@gmail.com>
2023-05-16 02:24:59 -0300

    [igogomes] Creating Project Card module and loading projects in Projects page from database

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -4,8 +4,12 @@ import Container from "../layout/Container"
 import LinkButton from "../layout/LinkButton"
 
 import styles from './Projects.module.css'
+import ProjectCard from "../project/ProjectCard"
+import { useEffect, useState } from "react"
 
 function Projects() {
+    const [projects, setProjects] = useState([]);
+
     const location = useLocation();
 
     let message = '';
@@ -13,6 +17,20 @@ function Projects() {
     if(location.state) {
         message = location.state.message;
     }
+
+    useEffect(() => {
+        fetch('http://localhost:5000/projects', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then(resp => resp.json())
+        .then((data) => {
+            console.log(data)
+            setProjects(data)
+        })
+        .catch(err => console.log(err))
+    }, []);
 
     return (
         <div className={styles.project_container}>
@@ -22,7 +40,9 @@ function Projects() {
             </div>
             {message && <Message type="success" msg={message} />}
             <Container customClass="start">
-                <p>Projetos</p>
+                {projects.length > 0 && projects.map((project) => (
+                    <ProjectCard name={project.name} id={project.id} budget={project.budget} category={project.category.name} key={project.id} />
+                ))}
             </Container>
         </div>
     );
