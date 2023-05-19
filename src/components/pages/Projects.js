9f5ad3b295ad3951f2d8d5a082acb240130c1bd3
Igo Gomes <igogomes0@gmail.com>
2023-05-19 01:19:34 -0300

    [igogomes] Applying structure to allow projects remove

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -12,6 +12,7 @@ import { useLocation } from 'react-router-dom'
 function Projects() {
     const [projects, setProjects] = useState([]);
     const [removeLoading, setRemoveLoading] = useState(false);
+    const [projectMessage, setProjectMessage] = useState('');
 
     const location = useLocation();
 
@@ -37,6 +38,19 @@ function Projects() {
         }, 3000)
     }, []);
 
+    function removeProject(id) {
+        fetch(`http://localhost:5000/projects/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }).then(resp => resp.json())
+        .then(() => {
+            setProjects(projects.filter((project) => project.id !== id));
+            setProjectMessage('Projecto removido com sucesso.');
+        }).catch(err => console.log(err));
+    }
+
     return (
         <div className={styles.project_container}>
             <div className={styles.title_container}>
@@ -44,9 +58,10 @@ function Projects() {
                 <LinkButton to="/newproject" text="Criar Projeto"/>
             </div>
             {message && <Message type="success" msg={message} />}
+            {projectMessage && <Message type="success" msg={projectMessage} />}
             <Container customClass="start">
                 {projects.length > 0 && projects.map((project) => (
-                    <ProjectCard name={project.name} id={project.id} budget={project.budget} category={project.category.name} key={project.id} />
+                    <ProjectCard name={project.name} id={project.id} budget={project.budget} category={project.category.name} key={project.id} handleRemove={removeProject} />
                 ))}
                 {!removeLoading && <Loading />}
                 {removeLoading && projects.length === 0 && (
--- src/components/project/ProjectCard.js
+++ src/components/project/ProjectCard.js
@@ -5,6 +5,11 @@ import { Link } from 'react-router-dom'
 import { BsPencil, BsFillTrashFill } from 'react-icons/bs'
 
 function ProjectCard({ id, name, budget, category, handleRemove }) {
+    const remove = (e) => {
+        e.preventDefault();
+        handleRemove(id);
+    }
+
     return (
         <div className={styles.project_card}>
             <h4>{name}</h4>
@@ -16,7 +21,7 @@ function ProjectCard({ id, name, budget, category, handleRemove }) {
             </p>
             <div className={styles.project_card_actions}>
                 <Link to='/'><BsPencil /> Editar</Link>
-                <button><BsFillTrashFill /> Excluir</button>
+                <button onClick={remove}><BsFillTrashFill /> Excluir</button>
             </div>
         </div>
     );
