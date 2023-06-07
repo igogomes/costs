49b9e064a21ae36b1b9e608bf05b9fc6c7de5f40
Igo Gomes <igogomes0@gmail.com>
2023-06-07 01:53:29 -0300

    [igogomes] Applying responsive adjustments for homepage

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,8 +1,12 @@
 import styles from './Footer.module.css'
 
 import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
+import moment from 'moment'
 
 function Footer() {
+    var getMoment = moment();
+    var currentYear = getMoment.format('YYYY');
+
     return (
         <footer className={styles.footer}>
             <ul className={styles.social_list}>
@@ -10,7 +14,7 @@ function Footer() {
                 <li><FaInstagram/></li>
                 <li><FaLinkedin/></li>
             </ul>
-            <p className={styles.copyright}><span>Costs</span> &copy; 2023</p>
+            <p className={styles.copyright}><span>Costs</span> &copy; {currentYear}</p>
         </footer>
     );
 }
