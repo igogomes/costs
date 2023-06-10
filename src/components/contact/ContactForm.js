67ee63eb481f93d7025a5546ec7e89468f049634
Igo Gomes <igogomes0@gmail.com>
2023-06-10 16:27:41 -0300

    [igogomes] Including subject field for contact form

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -1,15 +1,34 @@
-import styles from './ContactForm.module.css'
+import styles from './ContactForm.module.css' 
+
+import { useEffect, useState } from 'react'
 
 import Input from '../form/Input'
-import TextArea from '../form/TextArea';
+import TextArea from '../form/TextArea'
+import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 
 function ContactForm({ btnText }) {
+    const [subjects, setSubjects] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/subjects', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            setSubjects(data)
+        })
+        .catch(err => console.log(err));
+    }, []);
 
     return (
         <form className={styles.form}>
             <Input type="text" text="Digite seu nome" name="name" placeholder="Digite seu nome" />
             <Input type="email" text="Digite seu e-mail" name="email" placeholder="Digite seu e-mail" />
+            <Select name="subject" text="Selecione um assunto" options={subjects}/>
             <TextArea text="Digite sua mensagem" name="message" placeholder="Digite sua mensagem" />
             <SubmitButton text={btnText} />
         </form>
